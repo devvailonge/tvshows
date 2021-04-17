efb862bc38e8b8ba628587a280eb60c4a4796eb8
brunohensel <bruno.hensel@gmail.com>
2021-04-17 19:25:19 +0200

    Add bottom bar; create a commons module and add it as dependency in other modules.

--- app/src/main/java/com/devvailonge/tvshows/MainActivity.kt
+++ app/src/main/java/com/devvailonge/tvshows/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.devvailonge.tvshows
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.devvailonge.commons.viewBinding
+import com.devvailonge.tvshows.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+
+    private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::inflate)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupAppBarConfig()
+    }
+
+    override fun onSupportNavigateUp() =
+        Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
+
+    private fun setupAppBarConfig() {
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.topAppBar.setupWithNavController(navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+    }
 }
