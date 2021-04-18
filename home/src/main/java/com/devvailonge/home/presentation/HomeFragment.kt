80c67d1d7c9c29a3b1c8292d7c42c0bf0c484a4c
RenanWurster <totozerameister@gmail.com>
2021-04-18 16:23:48 +0200

    working on films list 2

--- home/src/main/java/com/devvailonge/home/presentation/HomeFragment.kt
+++ home/src/main/java/com/devvailonge/home/presentation/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.recyclerview.widget.RecyclerView
 import com.devvailonge.commons.collectIn
 import com.devvailonge.commons.viewBinding
 import com.devvailonge.home.R
@@ -23,7 +22,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private val viewModel: HomeViewModel by viewModels()
     private val binding: FragmentHomeBinding by viewBinding(FragmentHomeBinding::bind)
     private var categoryList = getCategories()
+    private var filmList = getFilms()
     private val adapter = CategoryAdapter(::categoryClickListener)
+    private val adapterFilm = FilmAdapter(filmList)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,6 +54,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.rvCategories.adapter = adapter
+        binding.rvFilms.adapter = adapterFilm
         adapter.updateList(categoryList)
     }
 
@@ -88,4 +90,40 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     }
 
     data class Category(val category: String, var isSelected: Boolean = false)
+
+
+    private fun getFilms(): List<Film> {
+        return arrayListOf(
+
+            Film(
+                "The Batman",
+                "https://i.pinimg.com/originals/c9/fe/b1/c9feb142ee2ac8d3c184ae58bd533f35.png"),
+
+            Film(
+                "Fast & Furious",
+                "https://static.cinecitta.de/portal/pics/berechnet/portal/pics/bilderdb/2021-04-01/2a0d318d-2cec-48dd-abeb-e4184105217c-_-_-141_Nz7jxgSmC7HuEouPhztWAwddEf8QFHluhCqMnrgUB4ShzjbVJ9nRd_L9NWvMqQ1vmfZxNpuYrUWtXXh_iw...jpg"),
+            Film(
+                "James Bond: No Time To Die",
+                "https://i.pinimg.com/originals/29/e7/23/29e7231164c94243f66e555c875ef3cb.jpg"),
+            Film(
+                "Top Gun 2",
+                "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Top_Gun_Maverick.jpg/220px-Top_Gun_Maverick.jpg"),
+            Film("Dune", "https://images-na.ssl-images-amazon.com/images/I/81A2o613bOL._RI_.jpg"),
+            Film("Justice League", "https://crops.giga.de/2f/b0/bc/602b1aafad6997a67dc9be3582_YyAxNDE5eDc5OCs4KzU2AnJlIDUwMCAyODADMjA2YmFkOTZjZTg=.jpg"),
+            Film("Black Mirror", ""),
+            Film("The King's Man", ""),
+            Film("Black Adam", ""),
+            Film("The Suicide Squad", ""),
+            Film("Rumble", ""),
+            Film("The Little Things", ""),
+
+
+            ).toList()
+    }
+
+
+    data class Film(val nome: String, val image: String)
 }
+
+
+
