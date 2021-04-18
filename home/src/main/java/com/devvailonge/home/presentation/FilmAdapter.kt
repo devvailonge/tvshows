e5e6221953eb37d67a8259a988b625bf21886ba0
RenanWurster <totozerameister@gmail.com>
2021-04-18 19:08:01 +0200

    integrated the API call and display the list

--- home/src/main/java/com/devvailonge/home/presentation/FilmAdapter.kt
+++ home/src/main/java/com/devvailonge/home/presentation/FilmAdapter.kt
@@ -5,15 +5,19 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.RoundedCornersTransformation
 import com.devvailonge.home.R
+import com.devvailonge.home.domain.Series
 import com.devvailonge.home.presentation.FilmAdapter.FilmViewHolder
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
-class FilmAdapter (private val films: List<HomeFragment.Film>)
-    : RecyclerView.Adapter<FilmViewHolder>() {
+class FilmAdapter() :
+    ListAdapter<Series, FilmViewHolder>(FilmAdapter) {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilmViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.item_film, parent, false)
@@ -21,36 +25,44 @@ class FilmAdapter (private val films: List<HomeFragment.Film>)
     }
 
     override fun onBindViewHolder(holder: FilmViewHolder, position: Int) {
-        holder.bind(films[position])
-    }
-
-    override fun getItemCount(): Int {
-        return films.size
+        holder.bind(getItem(position))
     }
 
 
 
-    class FilmViewHolder (itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    class FilmViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
         @ExperimentalCoroutinesApi
-        fun bind (data: HomeFragment.Film) {
+        fun bind(data: Series) {
             with(itemView) {
 
-            val ivFilm = findViewById<ImageView>(R.id.ivFilm)
-            val txtFilm = findViewById<TextView>(R.id.txtFilm)
-                txtFilm.text = data.nome
+                val ivFilm = findViewById<ImageView>(R.id.ivFilm)
+                val txtFilm = findViewById<TextView>(R.id.txtFilm)
+                txtFilm.text = data.name
 
 
-                    ivFilm.load(data.image) {
-                        crossfade(true)
-                        transformations(RoundedCornersTransformation( 15f))
-                    }
+                ivFilm.load(data.image.original) {
+                    crossfade(true)
+                    transformations(RoundedCornersTransformation(15f))
+                }
             }
         }
+    }
 
+        companion object : DiffUtil.ItemCallback<Series>() {
+            override fun areItemsTheSame(oldItem: Series, newItem: Series): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(oldItem: Series, newItem: Series): Boolean {
+                return oldItem == newItem
+            }
+
+        }
     }
 
 
-}
+
 
 
--- home/src/main/java/com/devvailonge/home/presentation/HomeFragment.kt
+++ home/src/main/java/com/devvailonge/home/presentation/HomeFragment.kt
@@ -10,6 +10,7 @@ import com.devvailonge.home.R
 import com.devvailonge.home.databinding.FragmentHomeBinding
 import com.devvailonge.home.domain.HomeState
 import com.devvailonge.home.domain.HomeSyncState
+import com.devvailonge.home.domain.Series
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.onEach
@@ -22,9 +23,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private val viewModel: HomeViewModel by viewModels()
     private val binding: FragmentHomeBinding by viewBinding(FragmentHomeBinding::bind)
     private var categoryList = getCategories()
-    private var filmList = getFilms()
     private val adapter = CategoryAdapter(::categoryClickListener)
-    private val adapterFilm = FilmAdapter(filmList)
+    private val adapterFilm = FilmAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +47,13 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             HomeSyncState.SeriesLoading -> {
             }
             HomeSyncState.SeriesSuccess -> {
+                handleSuccess(state.series)
             }
         }
+    }
+
+    private fun handleSuccess(series: List<Series>) {
+        adapterFilm.submitList(series)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -91,38 +96,6 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     data class Category(val category: String, var isSelected: Boolean = false)
 
-
-    private fun getFilms(): List<Film> {
-        return arrayListOf(
-
-            Film(
-                "The Batman",
-                "https://i.pinimg.com/originals/c9/fe/b1/c9feb142ee2ac8d3c184ae58bd533f35.png"),
-
-            Film(
-                "Fast & Furious",
-                "https://static.cinecitta.de/portal/pics/berechnet/portal/pics/bilderdb/2021-04-01/2a0d318d-2cec-48dd-abeb-e4184105217c-_-_-141_Nz7jxgSmC7HuEouPhztWAwddEf8QFHluhCqMnrgUB4ShzjbVJ9nRd_L9NWvMqQ1vmfZxNpuYrUWtXXh_iw...jpg"),
-            Film(
-                "James Bond: No Time To Die",
-                "https://i.pinimg.com/originals/29/e7/23/29e7231164c94243f66e555c875ef3cb.jpg"),
-            Film(
-                "Top Gun 2",
-                "https://upload.wikimedia.org/wikipedia/en/thumb/d/d2/Top_Gun_Maverick.jpg/220px-Top_Gun_Maverick.jpg"),
-            Film("Dune", "https://images-na.ssl-images-amazon.com/images/I/81A2o613bOL._RI_.jpg"),
-            Film("Justice League", "https://crops.giga.de/2f/b0/bc/602b1aafad6997a67dc9be3582_YyAxNDE5eDc5OCs4KzU2AnJlIDUwMCAyODADMjA2YmFkOTZjZTg=.jpg"),
-            Film("Black Mirror", ""),
-            Film("The King's Man", ""),
-            Film("Black Adam", ""),
-            Film("The Suicide Squad", ""),
-            Film("Rumble", ""),
-            Film("The Little Things", ""),
-
-
-            ).toList()
-    }
-
-
-    data class Film(val nome: String, val image: String)
 }
 
 
